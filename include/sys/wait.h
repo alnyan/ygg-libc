d1d49795ed8ac8614e61c8fee85ad4e4ca72a1ad
Mark <alnyan@airmail.cc>
2020-07-20 17:10:34 +0300

    Fix sys/wait.h header

--- include/sys/wait.h
+++ include/sys/wait.h
@@ -2,4 +2,11 @@
 #include <ygg/types.h>
 #include <ygg/wait.h>
 
+#define WIFEXITED(w)	(((w) & 0xff) == 0)
+#define WIFSIGNALED(w)	(((w) & 0x7f) > 0 && (((w) & 0x7F) < 0x7F))
+#define WIFSTOPPED(w)	(((w) & 0xff) == 0x7F)
+#define WEXITSTATUS(w)	(((w) >> 8) & 0xFF)
+#define WTERMSIG(w)	    ((w) & 0x7F)
+#define WSTOPSIG	    WEXITSTATUS
+
 pid_t waitpid(pid_t pid, int *status, int flags);
