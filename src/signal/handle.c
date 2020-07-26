6296f1b51eecc38732f015a53875f4cea6a93faf
Mark <alnyan@airmail.cc>
2020-07-26 20:57:47 +0300

    Proper exit status for signals

--- src/signal/handle.c
+++ src/signal/handle.c
@@ -2,9 +2,9 @@
 #include <_libc/signal.h>
 
 void _sig_dfl(int signum) {
-    exit(signum << 8);
+    exit((signum << 8) | signum);
 }
 
 void _sig_ign(int signum) {
-    exit(signum << 8);
+    exit((signum << 8) | signum);
 }
