4b20c66b6956a3c2fa23728b6c50a4162344b45b
Mark <alnyan@airmail.cc>
2020-07-26 20:58:48 +0300

    Implement signal-string conversion

--- src/string/strsignal.c
+++ src/string/strsignal.c
@@ -2,6 +2,31 @@
 #include <string.h>
 
 static const char *signal0[NSIG] = {
+    [SIGHUP]    = "Hangup",
+    [SIGINT]    = "Interrupt",
+    [SIGQUIT]   = "Quit",
+    [SIGILL]    = "Illegal instruction",
+    [SIGTRAP]   = "Trace/breakpoint trap",
+    [SIGABRT]   = "Abort",
+    [SIGBUS]    = "Bus error",
+    [SIGFPE]    = "Floating point exception",
+    [SIGKILL]   = "Killed",
+    [SIGUSR1]   = "User signal 1",
+    [SIGSEGV]   = "Segmentation fault",
+    [SIGUSR2]   = "User signal 2",
+    [SIGPIPE]   = "Broken pipe",
+    [SIGALRM]   = "Alarm clock",
+    [SIGTERM]   = "Terminated",
+    [SIGSYS]    = "Invalid system call",
+    [SIGCHLD]   = "Child exited",
+    [SIGCONT]   = "Continued",
+    [SIGSTOP]   = "Stopped",
+    [SIGTSTP]   = "Terminal stop",
+    [SIGTTIN]   = "Stopped (tty input)",
+    [SIGTTOU]   = "Stopped (tty output)",
+    [SIGURG]    = "Urgent I/O condition",
+    [SIGPWR]    = "Power fault",
+    [SIGWINCH]  = "Window size changed"
 };
 
 char *strsignal(int signum) {
