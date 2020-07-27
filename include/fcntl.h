a58c72d6729f9cc89d6579b430fbb20fb06943b5
Mark <alnyan@airmail.cc>
2020-07-27 18:19:39 +0300

    Follow POSIX open(2) signature

--- include/fcntl.h
+++ include/fcntl.h
@@ -2,5 +2,5 @@
 #include <ygg/fcntl.h>
 #include <ygg/types.h>
 
-int open(const char *pathname, int flags, mode_t mode);
+int open(const char *pathname, int flags, ...);
 int openat(int dfd, const char *pathname, int flags, mode_t mode);
--- src/fcntl/open.c
+++ src/fcntl/open.c
@@ -1,5 +1,13 @@
+#include <stdarg.h>
 #include <fcntl.h>
 
-int open(const char *pathname, int flags, mode_t mode) {
+int open(const char *pathname, int flags, ...) {
+    mode_t mode = 0;
+    if (flags & O_CREAT) {
+        va_list ap;
+        va_start(ap, flags);
+        mode = va_arg(ap, mode_t);
+        va_end(ap);
+    }
     return openat(AT_FDCWD, pathname, flags, mode);
 }
