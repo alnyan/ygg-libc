3967d63e30274c5a17648ed2d775dcfdb12fcf7c
Mark <alnyan@airmail.cc>
2020-07-28 17:57:18 +0300

    Print killer-signal to output

--- src/signal/handle.c
+++ src/signal/handle.c
@@ -1,7 +1,10 @@
-#include <stdlib.h>
 #include <_libc/signal.h>
+#include <sys/debug.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 void _sig_dfl(int signum) {
+    ygg_debug_trace("\033[41mKilled by signal %d\033[0m\n", signum);
     exit((signum << 8) | signum);
 }
 
