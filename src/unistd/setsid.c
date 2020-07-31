10269398e9492da1cd6af6dccbf547cf3b9717f7
Mark <alnyan@airmail.cc>
2020-07-31 15:26:20 +0300

    Add setsid(2)

--- src/unistd/setsid.c
+++ src/unistd/setsid.c
@@ -1,6 +1,8 @@
+#include <_libc/syscalls.h>
+#include <ygg/syscall.h>
 #include <unistd.h>
-#include <assert.h>
+#include <errno.h>
 
 pid_t setsid(void) {
-    assert(0 && "System call not yet implemented");
+    return SET_ERRNO(pid_t, __syscall0(SYSCALL_NR_SETSID));
 }
