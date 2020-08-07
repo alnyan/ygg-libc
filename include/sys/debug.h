3433634fe31d062f3ba7b649f7982714a14f7f54
Mark <alnyan@airmail.cc>
2020-08-07 12:07:05 +0300

    Update debug trace with new signature

--- include/sys/debug.h
+++ include/sys/debug.h
@@ -1,3 +1,5 @@
 #pragma once
+#include <stddef.h>
 
 void ygg_debug_trace(const char *msg, ...);
+void ygg_msg_trace(const char *data, size_t lim);
--- src/sys/debug.c
+++ src/sys/debug.c
@@ -3,17 +3,17 @@
 #include <sys/ioctl.h>
 #include <stdint.h>
 #include <stdarg.h>
+#include <stdio.h>
 #include <errno.h>
 
+void ygg_msg_trace(const char *msg, size_t lim) {
+    __syscall2(SYSCALL_NRX_TRACE, (long) msg, lim);
+}
+
 void ygg_debug_trace(const char *fmt, ...) {
-    uintptr_t a0, a1, a2, a3, a4;
+    char buf[512];
     va_list args;
     va_start(args, fmt);
-    a0 = va_arg(args, uintptr_t);
-    a1 = va_arg(args, uintptr_t);
-    a2 = va_arg(args, uintptr_t);
-    a3 = va_arg(args, uintptr_t);
-    a4 = va_arg(args, uintptr_t);
+    vsnprintf(buf, sizeof(buf), fmt, args);
     va_end(args);
-    __syscall6(SYSCALL_NRX_TRACE, (long) fmt, a0, a1, a2, a3, a4);
 }
