9e8fad9d70735e4032729478220b3e26317ee73e
Mark <alnyan@airmail.cc>
2020-08-24 11:33:31 +0300

    include: add inttypes.h

---
+++ include/inttypes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define PRId64      "ld"
+#define PRIu64      "lu"
+#define PRIx64      "lx"
+#define PRIX64      "lX"
+
+#define PRId32      "d"
+#define PRIx32      "x"
+#define PRIX32      "X"
+
+#define PRId16      "hd"
+#define PRIu16      "hu"
+#define PRIx16      "hx"
+#define PRIX16      "hX"
+
+#define PRId8       "hhd"
+#define PRIu8       "hhu"
+#define PRIx8       "hhx"
+#define PRIX8       "hhX"
